6b1224c6483b6b1dbc2e2e17a5c28f15c63a3175
Tayyib BASBUNAR <100427467+tbasdev57@users.noreply.github.com>
2025-06-01 13:15:16 +0200

    index.js

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ let result = document.getElementById("result");
 
 let getMovie = () => {
     let movieName = movieNameRef.value;
-    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+    let url = `https://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
 
     // Si le champ input est vide
 
